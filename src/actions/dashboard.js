d7240f3356d6a7cff45016e228a00a46b58356ad
Vishal Tyagi <vizzrg@gmail.com>
2025-10-04 13:17:46 +0530

    Developed industry insights dashboard

--- src/actions/dashboard.js
+++ src/actions/dashboard.js
@@ -48,6 +48,9 @@ export async function getIndustryInsights() {
     where: {
       clerkUserId: userId,
     },
+    include: {
+      industryInsight: true,
+    },
   });
 
   if (!user) throw new Error("User not found!!!");
